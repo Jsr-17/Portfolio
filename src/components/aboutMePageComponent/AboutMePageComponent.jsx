6c88685d62c90f3550643b9c95a97fe2dccc997e
Jsr-17 <123515664+Jsr-17@users.noreply.github.com>
2025-01-14 00:46:21 +0100

    Add files via upload

--- src/components/aboutMePageComponent/AboutMePageComponent.jsx
+++ src/components/aboutMePageComponent/AboutMePageComponent.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { HeaderComponent } from "../headerComponent";
 import { FooterComponent } from "../footerComponent";
+import { HeroAboutMePageComponent } from "./index";
 
 export const AboutMePageComponent = () => {
   return (
     <>
       <HeaderComponent></HeaderComponent>
+      <HeroAboutMePageComponent></HeroAboutMePageComponent>
 
       <FooterComponent></FooterComponent>
     </>
